a9666750c9525fb5a46b163dbf515dec949e5f54
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-04-16 04:17:59 -0600

    possibly add callback in progress

--- exllamav3/util/progress.py
+++ exllamav3/util/progress.py
@@ -1,12 +1,14 @@
 import sys
+from typing import Callable
 from rich.progress import Progress, BarColumn, TextColumn, TimeElapsedColumn, TimeRemainingColumn
 
 class ProgressBar:
 
-    def __init__(self, text: str, count: int, transient: bool = True):
+    def __init__(self, text: str, count: int, transient: bool = True, callback: Callable[[int, int], None] | None = None):
         self.text = text
         self.count = count
         self.transient = transient
+        self.callback = callback
         if self.text:
             self.progress = Progress(
                 TextColumn("[progress.description]{task.description}"),
@@ -22,6 +24,7 @@ class ProgressBar:
         if self.text:
             self.progress.start()
             sys.stdout.flush()
+        self.run_callback(0)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -29,11 +32,17 @@ class ProgressBar:
             if not self.transient:
                 self.progress.update(self.task_id, completed = self.count)
             self.progress.stop()
+        self.run_callback(self.count)
 
     def update(self, value: int):
         if self.text:
             self.progress.update(self.task_id, completed = value)
             sys.stdout.flush()
+        self.run_callback(value)
+
+    def run_callback(self, value: int):
+        if callback != None:
+            callback(value, self.count)
 
     def new_task(self, text: str, count: int):
         self.text = text
